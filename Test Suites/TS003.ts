51b2eb492d34e2b9427f8c4892ffd8d52c02362f
ZaimAl <zaimusna@gmail.com>
2023-05-04 07:19:08 +0700

    test

---
+++ Test Suites/TS003.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS003</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>baff1d02-5f95-4547-94c5-9ca451e61373</testSuiteGuid>
+</TestSuiteEntity>
